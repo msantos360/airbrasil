cb9cd63851d4a693df547f75fe4c4692f577a904
Michael Silva <michael.shel96@gmail.com>
2020-12-22 16:07:44 -0300

    first commit

---
+++ src/main/java/br/com/airbrasil/AirbrasilApplication.java
@@ -0,0 +1,13 @@
+package br.com.airbrasil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbrasilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbrasilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/airbrasil/AirbrasilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.airbrasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbrasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
